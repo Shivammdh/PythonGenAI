f1a152c2e758f1915a92a5f8936b1d191f0bb2e0
Shivam Sharma <shivamsharmamdh@gmail.com>
2026-01-12 19:00:59 +0530

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
+"""
+this is the program to get the data of .env file
+"""
 from langchain.agents import create_agent
 
 load_dotenv()   # 🔑 Loads .env file
